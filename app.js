5028e10a47572dab71813c69b178431de0695954
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-07-30 13:22:59 -0400

    properly formatted menu to make changes to selections to improve user convenience and clean up previous prompt

---
+++ app.js
@@ -0,0 +1,85 @@
+//Name:   Matt Keplinger
+//Org:    devCodeCamp
+//Title:  Day Trip Planner
+//Date:   30 July 2021
+
+"use strict"
+
+//Setup Arguments
+
+let destArray = ['Savannah', 'Charleston', 'Atlanta', 'Jacksonville', 'Brunswick'];
+let foodArray = ['Seafood', 'Burgers', 'BBQ', 'Food Truck Goodies', 'Soul Food'];
+let transArray = ['Motorcycle', 'Personal Car', 'Guided Tour Bus'];
+let funArray = ['Dance Club', 'Beach', 'Museum', 'Historic Landmark', 'Shopping Area'];
+
+//Execute Function
+let ranDest = confirmItem(destArray);
+let ranFood = confirmItem(foodArray);
+let ranTrans = confirmItem(transArray);
+let ranFun = confirmItem(funArray);
+let myTrip = confirmChoice();
+
+//DECLARE FUNCTIONS
+//Choose random index from array
+function randomItem(randomArray) {
+    let randomValue = randomArray[Math.floor(Math.random() * randomArray.length)];
+    return randomValue;
+}
+
+//Confirm that the user wants the random item 
+function confirmItem(randomArray) {
+    let makeChoice = randomItem(randomArray);
+    let confirmChoice = prompt(`Are you happy with ${makeChoice}? `);
+    if (confirmChoice === 'yes' || confirmChoice === 'y') {
+        console.log(`Item confirmed: ${makeChoice}`);
+    }
+    else {
+        confirmItem(randomArray);
+    }
+    return makeChoice;
+}
+
+//If user wants to make changes to the trip, allow for changes
+function makeNewChoice() {
+    let tryAgain = prompt(`Do you wish to make changes to your trip?`);
+    if (tryAgain === 'yes' || tryAgain === 'y') {
+        let userInput = prompt('Enter one of the following options (digit only):\n 1-Destination\n 2-Food\n 3-Transportation\n 4-Entertainment');
+        switch (userInput) {
+            case "1":
+                ranDest = confirmItem(destArray);
+                break;
+            case "2":
+                ranFood = confirmItem(foodArray);
+                break;
+            case "3":
+                ranTrans = confirmItem(transArray);
+                break;
+            case "4":
+                ranFun = confirmItem(funArray);
+                break;
+            default:
+                confirmItem();
+                confirmChoice();
+                break;
+        }
+    }
+}
+
+//Confirm user's final choices
+function confirmChoice() {
+    let confirmDecision = prompt('Do you wish to confirm the trip details?');
+    if (confirmDecision === 'no' || confirmDecision === 'n') {
+        makeNewChoice();
+        confirmChoice();
+    }
+    else {
+        showConfirmation();
+    }
+}
+
+//display output to show user options
+function showConfirmation() {
+    console.log(`Trip confirmed to ${ranDest}, eating ${ranFood}, traveling via ${ranTrans} and visiting a ${ranFun}.`);
+    }
+
+
